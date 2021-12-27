339edc8035156c3da3dc01e1aa9c9a18e303ce6a
Lami <Halam.Lee.Lami@gmail.com>
2021-12-27 16:34:09 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,41 @@
 'use strict';
 
-const home = (req, res) => {
-  res.render('home/index');
+const output = {
+  home: (req, res) => {
+    res.render('home/index');
+  },
+  login: (req, res) => {
+    res.render('home/login');
+  },
 };
 
-const login = (req, res) => {
-  res.render('home/login');
+const users = {
+  id: ['haha', '홍길동', '둘리'],
+  psword: ['hoho', '123', '또치'],
+};
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      psword = req.body.psword;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: '로그인에 실패하셨습니다.',
+    });
+  },
 };
 
 module.exports = {
-  home,
-  login,
+  output,
+  process,
 };
